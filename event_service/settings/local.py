088ca7edac90069d86ea05f2cd3368dc6ce254ac
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-02-13 19:19:39 -0400

    created: structure for event management service

---
+++ event_service/settings/local.py
@@ -0,0 +1,27 @@
+from .base import *
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv('DEBUG') == 'True'
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASS'),
+        'HOST': os.getenv('DB_HOST'),
+        'DATABASE_PORT': os.getenv('DB_PORT'),
+    }
+}

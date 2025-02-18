3b65091f8d9bff496e8fd38c8bb6b66aa12db424
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-02-17 20:00:41 -0400

    add: new dependency

--- event_service/settings/base.py
+++ event_service/settings/base.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import cloudinary
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +41,7 @@ LOCAL_APPS = [
 
 THIRD_APPS = [
     'rest_framework',
+    'cloudinary',
 ]
 
 INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
@@ -111,3 +116,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+cloudinary.config( 
+    cloud_name = "sample", 
+    api_key = os.getenv('CLOUD_KEY'), 
+    api_secret = os.getenv('CLOUD_SECRET'),
+    secure = True
+)

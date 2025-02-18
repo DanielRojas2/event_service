e57ef668a4a1195621a47b01aa8e34b328de04f3
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-02-17 20:22:13 -0400

    add: Artist model registered to admin panel

--- apis/event_info/admin.py
+++ apis/event_info/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models.artist import Artist
 
-# Register your models here.
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    search_fields = ['artist_country', 'artist_name']
+    list_display = [
+        'artist_name',
+        'artist_country',
+        'artist_image',
+        'created_at',
+        'updated_at'
+    ]

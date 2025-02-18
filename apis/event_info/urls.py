ecf3beeea9572d97a1ee8da50f8c24daa44c4343
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-02-17 20:23:58 -0400

    registered: view for Artist model

--- apis/event_info/urls.py
+++ apis/event_info/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
 from .views.location_view import LocationViewSet
+from .views.artist_view import ArtistListView
 
 router = DefaultRouter()
 router.register(r'locations', LocationViewSet, basename='locations')
+router.register(r'artists', ArtistListView, basename='artists')
 
 urlpatterns = router.urls
